2a746b52399813e8014fedbcfb525a31ac5b1bd0
Yuktha Shree P <yukthashree456@gmail.com>
2026-05-22 20:41:52 +0530

    ..

--- server/index.js
+++ server/index.js
@@ -96,7 +96,7 @@ app.use(
 );
 
 app.use(helmet());
-app.use(express.json({ limit: "10kb" }));
+app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 // Disable automatic ETag generation to avoid conditional 304 responses
 app.disable("etag");
@@ -180,6 +180,26 @@ app.use((err, req, res, next) => {
   }
   console.error("Unhandled error:", err.message);
   res.status(500).json({ error: "Something went wrong" });
+});
+app.use((err, req, res, next) => {
+
+  // Payload too large
+  if (err.type === "entity.too.large") {
+    return res.status(413).json({
+      success: false,
+      message: "Request payload is too large. Please reduce the size of your data."
+    });
+  }
+
+  // Invalid JSON
+  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid JSON format."
+    });
+  }
+
+  next(err);
 });
 
 app.listen(port, () => {
